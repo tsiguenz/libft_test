b1ae1db2432c5c086538de7409b8a34e4bcd3f0b
Thibaut SIGUENZA <tsiguenz@e1r2p21.42.fr>
2021-11-25 16:14:36 +0100

    addfront

--- lstaddfront_test.c
+++ lstaddfront_test.c
@@ -6,25 +6,48 @@
 /*   By: tsiguenz <tsiguenz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/25 15:00:30 by tsiguenz          #+#    #+#             */
-/*   Updated: 2021/11/25 15:00:59 by tsiguenz         ###   ########.fr       */
+/*   Updated: 2021/11/25 16:09:07 by tsiguenz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "test_libft.h"
-# define FILE "itoa"
+# define FILE "lstaddfront"
 # define PRINT_STR_TEST	print_str_test
 
-void	itoa_test(void)
+void	ft_print_result(t_list *elem)
 {
-	int i = 1;
+		int		len;
 
-	PRINT_STR_TEST(ft_itoa(5), "5", FILE, i++);
-	PRINT_STR_TEST(ft_itoa(-5), "-5", FILE, i++);
-	PRINT_STR_TEST(ft_itoa(42), "42", FILE, i++);
-	PRINT_STR_TEST(ft_itoa(-42), "-42", FILE, i++);
-	PRINT_STR_TEST(ft_itoa(0), "0", FILE, i++);
-	PRINT_STR_TEST(ft_itoa(-0), "0", FILE, i++);
-	PRINT_STR_TEST(ft_itoa(2147483647), "2147483647", FILE, i++);
-	PRINT_STR_TEST(ft_itoa(-2147483648), "-2147483648", FILE, i++);
+		len = 0;
+		while (((char *)elem->content)[len])
+					len++;
+		write(1, elem->content, len);
+		write(1, "\n", 1);
+}
+
+void	lstaddfront_test(void)
+{
+//	int i = 1;
+	t_list		*begin;
+	t_list		*elem;
+	t_list		*elem2;
+	t_list		*elem3;
+	t_list		*elem4;
+	char		*str = strdup("lorem");
+	char		*str2 = strdup("ipsum");
+	char		*str3 = strdup("dolor");
+	char		*str4 = strdup("sit");
+	
+	begin = NULL;
+	ft_lstadd_front(&begin, elem4);
+	ft_lstadd_front(&begin, elem3);
+	ft_lstadd_front(&begin, elem2);
+	ft_lstadd_front(&begin, elem)
 	printf("\n");
+	while (begin)
+	{
+		ft_print_result(begin);
+		begin = begin->next;
+		printf("\n");
+	}
 }
